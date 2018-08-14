ad5d0305c5e26626ae83b3ab13283f21a7fc480c
Yohan Rizk <yrizk@google.com>
2018-08-14 16:45:31 -0400

    adding cpp folder for clean room impls

---
+++ cpp/editor/main.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+/**
+ * Buffer class: holds the string
+ * Cursor class: holds the position of the cursor
+ *
+ *
+ */
+int main(int argc, char** argv) {}
+
+struct {
+} buffer;
